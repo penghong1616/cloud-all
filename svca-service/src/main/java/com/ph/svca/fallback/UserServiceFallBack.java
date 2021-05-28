11ef8a9509a72af31a89da83c60278f8512748d7
penghong <2391617997@qq.com>
2021-05-28 14:15:12 +0800

    OAuth2简易客户端搭建

--- svca-service/src/main/java/com/ph/svca/fallback/UserServiceFallBack.java
+++ svca-service/src/main/java/com/ph/svca/fallback/UserServiceFallBack.java
@@ -1,14 +1,17 @@
 package com.ph.svca.fallback;
 
 import com.ph.svca.service.UserService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * @Description: TODO
  * @author: penghong
  * @date: 2021/05/18/ 17:20
  */
-//@Component
+@Component
 public class UserServiceFallBack implements UserService {
+  @GetMapping("/info")
   @Override
   public String info() {
     return "业务忙，稍后获取";
--- svca-service/src/main/java/com/ph/svca/service/UserService.java
+++ svca-service/src/main/java/com/ph/svca/service/UserService.java
@@ -1,16 +1,18 @@
 package com.ph.svca.service;
 
+import com.ph.svca.fallback.UserServiceFallBack;
 import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @Description: TODO
  * @author: penghong
  * @date: 2021/05/18/ 16:45
  */
-@FeignClient(value = "user-service",path = "/user")
+@FeignClient(value = "user-service",path = "/user",fallback = UserServiceFallBack.class)
 public interface UserService {
-  @GetMapping("/info")
+  @RequestMapping(value = "/info",method = RequestMethod.GET)
   String info();
 
 }
